aab04e018ee34a68a48190f8a9e9aeafd062cf0c
Ayush Dixit <adixit@int6.local.snellius.surf.nl>
2024-07-27 13:03:21 +0200

    Add runcodes

--- runCodes_snellius_singleNode.sh
+++ runCodes_snellius_singleNode.sh
@@ -2,21 +2,39 @@
 
 #SBATCH -N 1
 #SBATCH --partition=genoa
-#SBATCH --ntasks-per-node=96
-#SBATCH --job-name=3008
+#SBATCH --ntasks-per-node=32
+#SBATCH --job-name=1010
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-type=ALL
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
 #SBATCH --mail-user=a.k.dixit@utwente.nl
 
 source ~/.bash_shell
 
 MAXlevel="9"
-Oh="1"
-Bo="0.05"
-offset="0.3"
-tmax="10"
+Oh=("1.00E-03" "3.00E-03" "6.00E-03" "1.00E-02" "3.00E-02" "6.00E-02" "1.00E-01")
+Bo="0.001"
+tmax="200"
+start_data=1010
 
-srun -n 96 asyBubbleinSheets_v1 $MAXlevel $Oh $filmThick $tmax
+start=1015
+end=1016
 
-#CC99='mpicc -std=c99' qcc -Wall -O2 -D_MPI=1 asyBubbleinSheets_v1.c -o asyBubbleinSheets_v1 -lm -disable-dimensions
+for (( i=start; i<=end; i++ )); do
+    index=$((i - start_data))
+    dir=$i
+    
+    cd $dir || { echo "Failed to change directory to $dir"; exit 1; }
+    
+    srun -n 16 --gres=cpu:16 --exclusive bubbleinSheets_01 $MAXlevel ${Oh[index]} $Bo $tmax &
+    
+    cd ../
+done
+
+wait
+
+#CC99='mpicc -std=c99' qcc -Wall -O2 -D_MPI=1 bubbleinSheets_01.c -o bubbleinSheets_01 -lm -disable-dimensions
+#CC99='mpicc -std=c99' qcc -Wall -O2 -D_MPI=1 asyBubbleinSheets_01.c -o asyBubbleinSheets_01 -lm -disable-dimensions
+#srun -n 96 asyBubbleinSheets_v1 $MAXlevel $Oh $offset $tmax
--- runpostProcess.sh
+++ runpostProcess.sh
@@ -2,6 +2,39 @@
 
 #SBATCH -N 1
 #SBATCH --partition=genoa
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=2:00:00
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
 
-python3 VideoAsyBubbleinSheet.py
+source ~/.bash_shell
+
+Oh=("1.00E-03" "3.00E-03" "6.00E-03" "1.00E-02" "3.00E-02" "6.00E-02" "1.00E-01")
+
+start_data=1010
+start=1015
+end=1016
+
+for (( i=start; i<=end; i++ )); do
+    dir=$i
+    index=$((i - start_data))  
+    cd $dir || { echo "Failed to change directory to $dir"; exit 1; }
+    
+    python3 VideoBubbleinSheet.py --Oh ${Oh[index]} &
+    
+    cd ../
+done
+
+wait
+
+for (( i=start; i<=end; i++ )); do
+    dir=$i/Video
+    cd $dir || { echo "Failed to change directory to $dir"; exit 1; }
+    
+    rm -f *.mp4
+    mkvids $i.mp4 &
+    
+    cd ../../
+done
+
+wait
